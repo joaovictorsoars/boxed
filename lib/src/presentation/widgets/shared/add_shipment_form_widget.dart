96717da7cf3f0d6f34accd0b46761cadd0efd522
João Victor <joao.victor.ssv@outlook.com>
2023-05-28 17:39:11 -0300

    fix: add shipments listing reset on a shipment delete.

--- lib/src/presentation/widgets/shared/add_shipment_form_widget.dart
+++ lib/src/presentation/widgets/shared/add_shipment_form_widget.dart
@@ -48,267 +48,267 @@ class _AddShipmentFormState extends State<AddShipmentFormWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          RichText(
-            text: const TextSpan(
-              text: 'Empresa ',
-              style: TextStyle(
-                color: BoxedColors.gray,
-                fontSize: 14,
-                fontWeight: FontWeight.w600,
-              ),
-              children: [
-                TextSpan(
-                  text: '*',
-                  style: TextStyle(
-                    color: BoxedColors.red,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                  ),
-                )
-              ],
-            ),
-          ),
-          const Divider(
-            height: 8,
-            color: BoxedColors.white,
-          ),
-          DropdownButtonFormField(
-            hint: const Text(
-              'Selecione uma empresa de entrega',
-              style: TextStyle(
-                fontSize: 14,
-              ),
-            ),
-            borderRadius: BorderRadius.circular(15),
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(
-                  width: 1.5,
-                  color: BoxedColors.grayLight,
-                ),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(
-                  width: 1.5,
-                  color: BoxedColors.redDark,
-                ),
-              ),
-              focusedErrorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(
-                  width: 1.5,
-                  color: BoxedColors.redDark,
-                ),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: BoxedColors.red,
-                  width: 1.5,
-                ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              prefixIcon: Icon(
-                PhosphorIcons.regular.buildings,
-                color: BoxedColors.gray,
-              ),
-            ),
-            icon: Icon(
-              PhosphorIcons.regular.caretDown,
-              color: BoxedColors.gray,
-            ),
-            items: companies
-                .map<DropdownMenuItem<String>>(
-                    (String value) => DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        ))
-                .toList(),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Este campo é obrigatório!';
-              }
-
-              return null;
-            },
-            onChanged: (value) {
-              setState(() {
-                actualCompany = value!;
-              });
-            },
-          ),
-          const Divider(
-            height: 12,
-            color: BoxedColors.white,
-          ),
-          RichText(
-            text: const TextSpan(
-              text: 'Código de rastreio ',
-              style: TextStyle(
-                color: BoxedColors.gray,
-                fontSize: 14,
-                fontWeight: FontWeight.w600,
-              ),
-              children: [
-                TextSpan(
-                  text: '*',
-                  style: TextStyle(
-                    color: BoxedColors.red,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
-                  ),
-                )
-              ],
-            ),
-          ),
-          const Divider(
-            height: 8,
-            color: BoxedColors.white,
-          ),
-          TextFormField(
-            controller: trackCodeController,
-            textCapitalization: TextCapitalization.characters,
-            cursorColor: BoxedColors.gray,
-            style: const TextStyle(fontSize: 14),
-            decoration: InputDecoration(
-              hintText: 'Digite o código de rastreio',
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
+    return BlocConsumer<ShipmentCorreiosCubit, ShipmentCorreiosState>(
+      listener: onShipmentListen,
+      builder: (context, state) => Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: const TextSpan(
+                text: 'Empresa ',
+                style: TextStyle(
                   color: BoxedColors.gray,
-                  width: 1.5,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
                 ),
-                borderRadius: BorderRadius.circular(15),
+                children: [
+                  TextSpan(
+                    text: '*',
+                    style: TextStyle(
+                      color: BoxedColors.red,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
               ),
-              border: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: BoxedColors.grayLight,
-                  width: 1.5,
-                ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusedErrorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(
-                  width: 1.5,
-                  color: BoxedColors.redDark,
-                ),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: BoxedColors.red,
-                  width: 1.5,
-                ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusColor: BoxedColors.gray,
-              prefixIcon: Icon(PhosphorIcons.regular.barcode),
-              prefixIconColor: BoxedColors.gray,
             ),
-            validator: (value) {
-              final regExp = RegExp(r'[a-zA-Z]{2}[0-9]{9}[a-zA-Z]{2}');
-
-              if (value == null || value.isEmpty) {
-                return 'Este campo é obrigatório!';
-              }
-
-              if (!regExp.hasMatch(value)) {
-                return 'O código inserido é inválido!';
-              }
-
-              return null;
-            },
-          ),
-          const Divider(
-            height: 12,
-            color: BoxedColors.white,
-          ),
-          RichText(
-            text: const TextSpan(
-              text: 'Apelido da remessa ',
-              style: TextStyle(
-                color: BoxedColors.gray,
-                fontSize: 14,
-                fontWeight: FontWeight.w600,
+            const Divider(
+              height: 8,
+              color: BoxedColors.white,
+            ),
+            DropdownButtonFormField(
+              hint: const Text(
+                'Selecione uma empresa',
+                style: TextStyle(
+                  fontSize: 14,
+                ),
               ),
-              children: [
-                TextSpan(
-                  text: '*',
-                  style: TextStyle(
-                    color: BoxedColors.red,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
+              borderRadius: BorderRadius.circular(15),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    width: 1.5,
+                    color: BoxedColors.grayLight,
                   ),
-                )
-              ],
-            ),
-          ),
-          const Divider(
-            height: 8,
-            color: BoxedColors.white,
-          ),
-          TextFormField(
-            controller: nickNameController,
-            cursorColor: BoxedColors.gray,
-            style: const TextStyle(fontSize: 14),
-            decoration: InputDecoration(
-              hintText: 'Digite o apelido da remessa',
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    width: 1.5,
+                    color: BoxedColors.redDark,
+                  ),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    width: 1.5,
+                    color: BoxedColors.redDark,
+                  ),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: BoxedColors.red,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                prefixIcon: Icon(
+                  PhosphorIcons.regular.buildings,
                   color: BoxedColors.gray,
-                  width: 1.5,
-                ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              border: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: BoxedColors.grayLight,
-                  width: 1.5,
-                ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusedErrorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-                borderSide: const BorderSide(
-                  width: 1.5,
-                  color: BoxedColors.redDark,
                 ),
               ),
-              errorBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: BoxedColors.red,
-                  width: 1.5,
-                ),
-                borderRadius: BorderRadius.circular(15),
+              icon: Icon(
+                PhosphorIcons.regular.caretDown,
+                color: BoxedColors.gray,
               ),
-              focusColor: BoxedColors.gray,
-              prefixIcon: Icon(PhosphorIcons.regular.pencil),
-              prefixIconColor: BoxedColors.gray,
+              items: companies
+                  .map<DropdownMenuItem<String>>(
+                      (String value) => DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          ))
+                  .toList(),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Este campo é obrigatório!';
+                }
+
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  actualCompany = value!;
+                });
+              },
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Este campo é obrigatório!';
-              }
+            const Divider(
+              height: 12,
+              color: BoxedColors.white,
+            ),
+            RichText(
+              text: const TextSpan(
+                text: 'Código de rastreio ',
+                style: TextStyle(
+                  color: BoxedColors.gray,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
+                children: [
+                  TextSpan(
+                    text: '*',
+                    style: TextStyle(
+                      color: BoxedColors.red,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              height: 8,
+              color: BoxedColors.white,
+            ),
+            TextFormField(
+              controller: trackCodeController,
+              textCapitalization: TextCapitalization.characters,
+              cursorColor: BoxedColors.gray,
+              style: const TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                hintText: 'Digite o código de rastreio',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: BoxedColors.gray,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: BoxedColors.grayLight,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    width: 1.5,
+                    color: BoxedColors.redDark,
+                  ),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: BoxedColors.red,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusColor: BoxedColors.gray,
+                prefixIcon: Icon(PhosphorIcons.regular.barcode),
+                prefixIconColor: BoxedColors.gray,
+              ),
+              validator: (value) {
+                final regExp = RegExp(r'[a-zA-Z]{2}[0-9]{9}[a-zA-Z]{2}');
 
-              if (value.length < 5) {
-                return 'O apelido deve ter ao menos 5 caracteres!';
-              }
+                if (value == null || value.isEmpty) {
+                  return 'Este campo é obrigatório!';
+                }
 
-              return null;
-            },
-          ),
-          const Divider(
-            height: 12,
-            color: BoxedColors.white,
-          ),
-          BlocConsumer<ShipmentCorreiosCubit, ShipmentCorreiosState>(
-            listener: onShipmentListen,
-            builder: (context, state) => InkWell(
+                if (!regExp.hasMatch(value)) {
+                  return 'O código inserido é inválido!';
+                }
+
+                return null;
+              },
+            ),
+            const Divider(
+              height: 12,
+              color: BoxedColors.white,
+            ),
+            RichText(
+              text: const TextSpan(
+                text: 'Apelido da remessa ',
+                style: TextStyle(
+                  color: BoxedColors.gray,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
+                children: [
+                  TextSpan(
+                    text: '*',
+                    style: TextStyle(
+                      color: BoxedColors.red,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Divider(
+              height: 8,
+              color: BoxedColors.white,
+            ),
+            TextFormField(
+              controller: nickNameController,
+              cursorColor: BoxedColors.gray,
+              style: const TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                hintText: 'Digite o apelido da remessa',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: BoxedColors.gray,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: BoxedColors.grayLight,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: const BorderSide(
+                    width: 1.5,
+                    color: BoxedColors.redDark,
+                  ),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                    color: BoxedColors.red,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                focusColor: BoxedColors.gray,
+                prefixIcon: Icon(PhosphorIcons.regular.pencil),
+                prefixIconColor: BoxedColors.gray,
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Este campo é obrigatório!';
+                }
+
+                if (value.length < 5) {
+                  return 'O apelido deve ter ao menos 5 caracteres!';
+                }
+
+                return null;
+              },
+            ),
+            const Divider(
+              height: 12,
+              color: BoxedColors.white,
+            ),
+            InkWell(
               onTap: actualCompany.isNotEmpty ? submitForm : null,
               borderRadius: BorderRadius.circular(15),
               splashColor: BoxedColors.grayLight,
@@ -332,8 +332,8 @@ class _AddShipmentFormState extends State<AddShipmentFormWidget> {
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
